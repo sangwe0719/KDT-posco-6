f761327a9c5f47d92cd340b01ede3032aa3c8528
마상위 <ma0719@naver.com>
2023-04-01 10:02:18 +0900

    study: multer 세부설정

--- class/5주차/2023.03.30/practice/app.js
+++ class/5주차/2023.03.30/practice/app.js
@@ -23,7 +23,7 @@ app.post('/axios', function (req, res) {
   console.log(req.body); // { userId: 'aa', userPw: 'a' }
 
   // 정답 아이디/비밀번호 vs. 사용자가 폼에 입력한 아이디/비밀번호
-  if (realId === req.body.userId && realPw === req.body.userPw) {
+  if (realId === req.body.id && realPw === req.body.pw) {
     // 정답 아이디랑 사용자가 입력한 아이디가 같고, 정답 비번이랑 사용자가 입력한 비번이 같음
     // => 로그인 성공
     res.send({ isLogin: true, userInfo: req.body });
--- class/5주차/2023.03.31/10_file_upload/app.js
+++ class/5주차/2023.03.31/10_file_upload/app.js
@@ -7,7 +7,29 @@ const multer = require('multer'); // multer 불러오기
 const path = require('path');// path 불러오기 (내장 모듈) => 파일, 폴더 경로를 쉽게 설정
 const upload = multer({
     dest: 'uploads/' , // dest: 업로드할 파일 경로 지정
-})
+});
+// multer 세부 설정
+const uploadDetail = multer({
+    storage: multer.diskStorage ({
+        destination(req,file,done){
+            // destination: 경로설정
+            // done: callback 함수
+            done(null,'uploads/');    
+        },
+        filename(req,file,done){
+            // 가정) apple.png 파일을 업로드
+            const ext = path.extname(file.originalname); // file.orginalname에서 "확장자" 추출
+            // path.basename(file.originalname, ext) => apple(확장자 제거한 파일이름만!!)
+            // Date.now() => 현재 시간 (1680309362206)
+            // => 1970년 1월 1일 0시 0분 0초를 기준으로 현재까지 경과된 밀리초
+            done(null,path.basename(file.originalname, ext) + Date.now() + ext)
+            //(파일명 + 현재시간.확장자) 형식으로 파일 업로드
+        },
+    }),
+    limits: {fileSize: 5 * 1024 * 1024}, //5MB
+    // 5 * 2^10 = 5KB
+    // 5 * 2^10 * 2^10 = 5MB
+});
 
 app.set('view engine','ejs')
 app.use('/views',express.static(__dirname + '/views'));
@@ -19,10 +41,9 @@ app.get('/',(req,res)=>{
     res.render('index');
 });
 
-
 // single(): 하나의 파일 업로드할 때
 // single()의 매개변수: input 의 name과 일치시키기!
-app.post('/upload' , upload.single('userfile'), (req, res) =>{
+app.post('/upload' , uploadDetail.single('userfile'), (req, res) =>{
     console.log(req.file); // 업로드한 파일 정보
     // {
     //     fieldname: 'userfile', // 폼에 정의한 name
